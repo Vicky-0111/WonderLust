a1f60c8c6e8579a0a505ba75ab3e2c83687ba289
Vicky-0111 <sanzujack7@gmail.com>
2026-06-22 22:35:34 +0530

    Fixed: Add start script and dynamic port for Render Deployment

--- app.js
+++ app.js
@@ -122,7 +122,8 @@ app.use((err, req, res, next) => {
     //res.status(statusCode).send(message);
 });
 
-app.listen(8080, () => {
-    console.log("Server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
 })
 
